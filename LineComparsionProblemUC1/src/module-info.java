323c7ecde33dba2ffa378e2a2057d467dcee1a91
Mohammad Ali Khan <79047819+M181993@users.noreply.github.com>
2021-06-09 00:36:19 +0530

    LineComparsionProblemUC1

---
+++ LineComparsionProblemUC1/src/module-info.java
@@ -0,0 +1,2 @@
+module LineComparsionProblemUC1 {
+}
---
+++ LineComparsionProblemUC1/src/usecase1/LineComparsionProblemUC1.java
@@ -0,0 +1,15 @@
+package usecase1;
+
+public class LineComparsionProblemUC1 {
+	
+	public static void main(String arg[]) {
+		int x1, x2, y1, y2;
+		double dis;
+		x1=1;
+		x2=3;
+		y1=2;
+		y2=-6;
+		dis = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		System.out.println("length" + "("+x1+", "+y1+"), "+" ("+x2+", "+y2+") =" + dis);
+	}
+}
